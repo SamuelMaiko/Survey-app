4a93989c7b843a53afd8708f04f73fab551bac0d
Samuel Maiko <sammymaiko721@gmail.com>
2024-01-17 00:23:11 +0300

    pip updated

--- myfirst/settings.py
+++ myfirst/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = 'django-insecure-lbn8+5enk$28di1x74q9$n9--ih4y$ng8j944l@ue(yy59oaex
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'elwanex-survey-app.onrender.com']
 
 
 # Application definition
